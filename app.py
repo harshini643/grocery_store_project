31fd1015be20ed7796ede91c6f585eaad2db8594
harshini643 <sriharshukudupudi23@gmail.com>
2025-09-03 18:28:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate=Migrate(app,db)
-
+with app.app_context():
+    db.create_all()
 # -------------------- Models --------------------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)

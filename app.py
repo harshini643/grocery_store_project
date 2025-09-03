71a28c1bf2e142c5b6ca3d747fbc45e5828872f3
harshini643 <sriharshukudupudi23@gmail.com>
2025-09-03 19:12:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate=Migrate(app,db)
 
-@app.before_first_request
+@app.before_serving
 def create_tables():
     db.create_all()
 
